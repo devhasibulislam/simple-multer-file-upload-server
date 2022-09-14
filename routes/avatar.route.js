b64aa32076d43bddf655f4e8be640eb3d398db70
hasibulislam999 <hasib143sl@gmail.com>
2022-09-14 10:26:15 +0600

    avatar route added

---
+++ routes/avatar.route.js
@@ -0,0 +1,16 @@
+// external import
+const express = require("express");
+const multer = require("multer");
+
+// internal import
+const postAnAvatar = require("../controllers/avatar.controller");
+
+// upload variable
+const upload = multer({ dest: `${__dirname}/../avatars/` });
+
+// router connection
+const router = express.Router();
+
+router.post("/", upload.single("avatar"), postAnAvatar);
+
+module.exports = router;
