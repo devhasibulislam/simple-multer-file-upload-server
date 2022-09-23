fdff831d913e3112e29cd9b1e234edf7d7ca507e
hasibulislam999 <hasib143sl@gmail.com>
2022-09-23 20:42:09 +0600

    mongodb connected

--- utils/db.util.js
+++ utils/db.util.js
@@ -4,10 +4,14 @@ const mongoose = require("mongoose");
 // DB connect with Mongoose
 function dbConnection() {
   mongoose
-    .connect("mongodb://localhost:27017/file-upload", {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    })
+    .connect(
+      "mongodb+srv://Tour_Management_System:2a7f3b0d38c318df@tms.o7mokk2.mongodb.net/?retryWrites=true&w=majority",
+      {
+        dbName: "file-upload",
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      }
+    )
     .then(() => {
       console.log("Successfully connect Mongoose with MongoDB.");
     });
